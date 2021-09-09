16a5bb53e381966b0bfae2b27502b9a00b1c5988
Your Name <you@example.com>
2021-09-09 16:41:34 +0700

    A

--- code/quy-tu-thien/resources/views/home/confirm.blade.php
+++ code/quy-tu-thien/resources/views/home/confirm.blade.php
@@ -50,7 +50,7 @@
         <div class="col-sm-4">
             <div  style="transform: rotate(-90deg);">
                     @foreach ($listimg as $row)
-                    <img style="width: 100%; height: 100;" src="{{$row}}" ><br>
+                    <img style="width: 100%;" src="{{$row}}" ><br>
                     @endforeach
             </div>
         </div>
